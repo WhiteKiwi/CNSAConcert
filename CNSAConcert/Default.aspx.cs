2eff788e24a52e79121519e4032a646cad28c707
장지훈 <s5v7@outlook.kr>
2018-05-13 16:47:26 +0900

    Login Session 검사
    Login이 되어있지 않을 시 로그인 페이지로 Redirect

--- CNSAConcert/Default.aspx.cs
+++ CNSAConcert/Default.aspx.cs
@@ -3,7 +3,9 @@
 namespace CNSAConcert {
 	public partial class Default : System.Web.UI.Page {
 		protected void Page_Load(object sender, EventArgs e) {
-
+			// Redirect to the Login page if the session does not exist
+			if (String.IsNullOrEmpty((string)Session["StudentNumber"]) || String.IsNullOrEmpty((string)Session["Grade"]))
+				Response.Redirect("/Login.aspx");
 		}
 	}
 }
