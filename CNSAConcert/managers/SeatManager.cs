ff6ca790ef6b0c2ead83c9b1012f13f55794aa67
장지훈 <s5v7@outlook.kr>
2018-05-13 15:35:31 +0900

    Add SeatManager

---
+++ CNSAConcert/managers/SeatManager.cs
@@ -0,0 +1,10 @@
+﻿using CNSAConcert.Models;
+using MySql.Data.MySqlClient;
+
+namespace CNSAConcert.Managers {
+	public class SeatManager {
+		// Table Name
+		private const string SEATTABLE = "seats";
+
+	}
+}
