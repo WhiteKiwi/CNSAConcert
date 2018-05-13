8043d3562af4d8f4315fc45bbf4c86adb2410ded
장지훈 <s5v7@outlook.kr>
2018-05-14 08:27:05 +0900

    계정 관리 페이지 추가
    비밀번호 변경

---
+++ CNSAConcert/Account.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Account.aspx.cs" Inherits="CNSAConcert.Account" %>
+
+<!DOCTYPE html>
+
+<html>
+<head runat="server">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title></title>
+</head>
+<body>
+	<form runat="server">
+		<asp:Textbox runat="server" ID="OldPassword" TextMode="Password"></asp:Textbox><br />
+		<asp:Textbox runat="server" ID="NewPassword" TextMode="Password"></asp:Textbox><br />
+		<asp:Textbox runat="server" ID="NewPassword2" TextMode="Password"></asp:Textbox><br />
+		<br />
+		<asp:Button runat="server" ID="ApplyButton" Text="Apply" OnClick="ApplyButton_Click" />
+	</form>
+</body>
+</html>
---
+++ CNSAConcert/Account.aspx.cs
@@ -0,0 +1,31 @@
+﻿using CNSAConcert.Managers;
+using CNSAConcert.Models;
+using System;
+
+namespace CNSAConcert {
+	public partial class Account : System.Web.UI.Page {
+		protected void Page_Load(object sender, EventArgs e) {
+			// Redirect to the Login page if the session does not exist
+			if (String.IsNullOrEmpty((string)Session["StudentNumber"]) || String.IsNullOrEmpty((string)Session["Grade"]))
+				Response.Redirect("/Login.aspx");
+		}
+
+		protected void ApplyButton_Click(object sender, EventArgs e) {
+			// Change password if text in 'newPassword' textbox is same with text in 'newPassword2' textbox
+			if (NewPassword.Text == NewPassword2.Text) {
+                // Fail to change password if return value of method 'ChangePassword' is -1
+				if (UserManager.ChangePassword(new User {
+					StudentNumber = (string)Session["StudentNumber"],
+					Password = OldPassword.Text
+				}, NewPassword.Text) == -1) {
+					Response.Write("<script>alert('Please confirm the original password');</script>");
+				} else {
+					Response.Write("<script>alert('Success to change password');</script>");
+				}
+			} else {
+				// Notify If passwords do not match
+				Response.Write("<script>alert('Passwords do not match');</script>");
+			}
+		}
+	}
+}
