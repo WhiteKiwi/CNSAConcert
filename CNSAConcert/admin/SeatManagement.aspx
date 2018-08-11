f5bd691263d0755de47d01d67c98a59e43a8e39c
장지훈 <s5v7@outlook.kr>
2018-08-11 11:07:12 +0900

    좌석 추가 버튼 추가

---
+++ CNSAConcert/admin/SeatManagement.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SeatManagement.aspx.cs" Inherits="CNSAConcert.admin.SeatManagement" %>
+
+<!DOCTYPE html>
+
+<html>
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form runat="server">
+		<h1>학년</h1>
+		<asp:TextBox runat="server" ID="Grade"></asp:TextBox>
+		<h1>Row</h1>
+		<asp:TextBox runat="server" ID="Row"></asp:TextBox>
+		<h1>Col</h1>
+		<asp:TextBox runat="server" ID="Col"></asp:TextBox>
+		<asp:Button runat="server" ID="AddButton" OnClick="AddButton_Click" Text="추가" />
+    </form>
+</body>
+</html>
---
+++ CNSAConcert/admin/SeatManagement.aspx.cs
@@ -0,0 +1,36 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace CNSAConcert.admin {
+	public partial class SeatManagement : System.Web.UI.Page {
+		protected void Page_Load(object sender, EventArgs e) {
+			if (Session["ISLOGIN"] == null) {
+				Response.Redirect("/admin/Login.aspx");
+			}
+		}
+
+		protected void AddButton_Click(object sender, EventArgs e) {
+			// Connect to DB
+			using (var conn = new MySqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["ConcertDB"].ConnectionString)) {
+				conn.Open();
+
+				// Command Text - Delete Student Number
+				var cmd = new MySqlCommand("", conn);
+
+				int row = int.Parse(Row.Text);
+				int col = int.Parse(Col.Text);
+				for (int i = 1; i <= row; i++) {
+					for (int j = 1; j <= col; j++) {
+						cmd.CommandText = $"INSERT INTO seats{Grade.Text}(Row, Col) VALUES ('{i}', '{j}');";
+
+						// The number of rows affected
+						cmd.ExecuteNonQuery();
+					}
+				}
+
+				// Connection Close
+				conn.Close();
+			}
+		}
+	}
+}
